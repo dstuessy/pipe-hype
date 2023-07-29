b8428d3ad748f4f854063214e4f7ce2dfe8940a9
Daniel Stuessy <danielstuessy@gmail.com>
2023-07-29 11:03:33 +0100

    render a cell

--- main.js
+++ main.js
@@ -4,8 +4,27 @@ if (!PIXI.utils.isWebGLSupported()) {
   type = "canvas";
 }
 
+//Aliases
+const Application = PIXI.Application,
+    Assets = PIXI.Assets,
+    Sprite = PIXI.Sprite;
+
 //Create a Pixi Application
-const app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
+const app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+app.resizeTo = window;
+
+app.renderer.backgroundColor = 0xffffff;
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
+
+;(async () => {
+  const texture = await Assets.load("assets/cell.png");
+  const image = Sprite.from(texture);
+
+  app.stage.addChild(image);
+})()
